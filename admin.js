6daef98b8b46fa191c32443dfe42d46eb217545d
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-04-18 16:50:01 +0200

    Blog

--- admin.js
+++ admin.js
@@ -1,6 +1,29 @@
-var blog = [];
+window.onload=function() {
+  horloge('div_horloge');
+};
+ 
+function horloge(el) {
+  if(typeof el=="string") { el = document.getElementById(el); }
+  function actualiser() {
+    var date = new Date();
+    var str = date.getHours();
+    str += ':'+(date.getMinutes()<10?'0':'')+date.getMinutes();
+    str += ':'+(date.getSeconds()<10?'0':'')+date.getSeconds();
+    el.innerHTML = str;
+  }
+  actualiser();
+  setInterval(actualiser,1000);
+}
+
+
+
 var titre = ""
 var text = ""
+
+
+
+
+//-- markdown
 
 $("#titre").keyup(function(){
 	titre = $("#titre").val();
@@ -12,63 +35,88 @@ $("#text").keyup(function(){
 
 });	
 
+
+
+//-- Administration
+
 $("#button").click(function(){
 
 	var contenu = {"titre" : titre, "text" : text}
 	$("input").val("");
 	$("textarea").val("");
-	blog.push(contenu);
 
   $.ajax({ 
     url:'http://192.168.1.50/json-db',
      data: { 
       task: 'set', 
       key: 'MouadBlog', 
-      value: JSON.stringify(blog),
+      value: JSON.stringify(contenu),
     } 
-  
   });
-
 });
-
-
-function Blog(tab){
- 	for (var i = 0; i < tab.length; i++) {
- 		$("#title").append('<option>'+tab[i][i].titre+'</option>');
- 		$("#info").append('<div>'+tab[i][i].text+'</div>');
- 	}
-}
-
-
-
-function charger(){
-	$.ajax({ 
-		url:'http://192.168.1.50/json-db', 
-		data: { 
-			task: 'get',
-			key: 'MouadBlog', 
-			} 
-	})
-	.done(function(data){
-
-		blog=JSON.parse(data);
-		console.log(blog);
-		Blog(blog);
-	});	
-
-			
-   //console.log(data);
-}
-charger();
 
 $("#text").keyup(function(){
 	
-var converter = new showdown.Converter(),
-    text      = $('#text').val(),
-    html      = converter.makeHtml(text);
+	var converter = new showdown.Converter(),
+   	 	text      = $('#text').val(),
+   	 	html      = converter.makeHtml(text);
 	
 	$("#texthtml").html(html);
 });
-    //console.log(html);
 
 
+
+
+
+//-- Site public
+
+$.ajax({ 
+	url:'http://192.168.1.50/json-db', 
+	data: { 
+		task: 'get',
+		key: 'MouadBlog', 
+		} 
+})
+.done(function(data){
+
+	tab=JSON.parse(data);
+	console.log(tab);
+
+ 	for (var i = 0; i < tab.length; i++) {
+ 		$("#title").append('<option value="ID'+i+'">'+tab[i].titre+'</option>');
+ 		$("#info").append('<div class="ID'+i+'">'+tab[i].text+'</div>');
+ 	}
+});	
+
+
+// $('#title').on('change', function(){
+// 	alert('changé !');
+
+
+// });
+
+
+
+
+
+
+ // $.ajax({
+	// url:'http://192.168.1.50/json-db',
+ // 	data: {
+ // 		task: 'delete',
+ // 		key: 'MouadBlog'
+ // 	}
+ // });
+	
+
+
+
+// $.ajax({
+//   url:'http://192.168.1.50/json-db',
+//   data: {
+//     task: 'get',
+//     key: 'MouadBlog'
+//     },
+//    success: function(data){
+//    }
+//   });
